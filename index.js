ec94755ee179b473a6c7b59280bfbd8f9ad2603d
roe <rowen.floarea@proton.me>
2025-08-11 05:56:18 -0400

    connect to db (very minimal)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const port = 3000;
 
 app.get('/', (req, res) => {
@@ -8,4 +9,7 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
 	console.log(`App listening on port: ${port}`);
-})
+});
+
+mongoose.connect('mongodb://127.0.0.1:27017/libtickit')
+	.then(() => console.log('Connected to database'));
